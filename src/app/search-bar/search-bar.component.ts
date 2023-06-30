b5747d7864b2cb41b7ba66ddddcf62a167509f6b
pulpetto <116948697+pulpetto@users.noreply.github.com>
2023-06-30 20:47:54 +0200

    child to parent communication

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
     selector: 'app-search-bar',
     templateUrl: './search-bar.component.html',
     styleUrls: ['./search-bar.component.css'],
 })
-export class SearchBarComponent {}
+export class SearchBarComponent {
+    @Output() submitted = new EventEmitter<string>();
+
+    term = '';
+
+    onFormSubmit(event: any) {
+        event.preventDefault();
+        this.submitted.emit(this.term);
+    }
+}
