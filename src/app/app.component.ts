5b249e23b06411498738f76da1ba05895d43100e
pulpetto <116948697+pulpetto@users.noreply.github.com>
2023-07-02 09:41:34 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,6 @@ export class AppComponent {
     constructor(private wikipedia: WikipediaService) {}
 
     onTerm(term: string) {
-        const results = this.wikipedia.search(term);
+        this.wikipedia.search(term).subscribe((response) => {});
     }
 }
