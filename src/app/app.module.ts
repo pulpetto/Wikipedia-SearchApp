d55737334f8104887f605939a56ba0269e084ae9
pulpetto <116948697+pulpetto@users.noreply.github.com>
2023-07-02 09:33:26 +0200

    accepting request

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
 import { PageListComponent } from './page-list/page-list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    SearchBarComponent,
-    PageListComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent, SearchBarComponent, PageListComponent],
+    imports: [BrowserModule, HttpClientModule],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/wikipedia.service.ts
+++ src/app/wikipedia.service.ts
@@ -1,12 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
     providedIn: 'root',
 })
 export class WikipediaService {
-    constructor() {}
+    constructor(private http: HttpClient) {}
 
     public search(term: string) {
-        return 'wikipedia search result';
+        return this.http.get(
+            'https://en.wikipedia.org/w/api.php?origin=*&action=opensearch&search=space&limit=5&namespace=0&format=json',
+            {
+                params: {
+                    action: 'query',
+                    format: 'json',
+                    list: 'search',
+                    utf: '1',
+                    srsearch: term,
+                    origin: '*',
+                },
+            }
+        );
     }
 }
